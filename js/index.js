e91f212ba5a72010f8bad1f0b4637ae16b7d2dbb
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-26 12:14:00 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,20 +5,20 @@ let element = document.createElement('div');
 
 element.innerHTML = 'Hello, DOM!'
 
-> element.style.backgroundColor = '#f9f9f9'
+element.style.backgroundColor = '#f9f9f9'
 
-> document.body.appendChild(element);
+document.body.appendChild(element);
 
-> element.style.textAlign = 'center';
+element.style.textAlign = 'center';
 
-> let ul = document.createElement('ul');
+let ul = document.createElement('ul');
 
-> for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 3; i++) {
   let li = document.createElement('li');
   li.innerHTML = (i + 1).toString();
   ul.appendChild(li);
 }
 
-> element.appendChild(ul);
+element.appendChild(ul);
 
-> element.style.textAlign = 'left';
+element.style.textAlign = 'left';
